3974d67ff4684ae8bac4ee1434e34d15f172a27f
Edward Kmett <ekmett@gmail.com>
2012-08-31 12:01:13 -0400

    principled use of unsafeCoerce

--- src/Indexed/Thrist.hs
+++ src/Indexed/Thrist.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE TypeFamilies #-}
 module Indexed.Thrist
   ( Thrist(..)
   , List
@@ -16,32 +17,43 @@ import Indexed.Types
 import Unsafe.Coerce
 
 infixr 5 :-
+
+-- derpendent types
+derp :: (Fst ij ~ i, Snd ij ~ j) => p ij -> p '(i,j)
+derp = unsafeCoerce
+
+herp :: (Fst ij ~ i, Snd ij ~ j) => p '(i,j) -> p ij
+herp = unsafeCoerce
+
 -- A Thrist.
 data Thrist :: ((i,i) -> *) -> (i,i) -> * where
   Nil :: Thrist a '(i,i)
-  (:-) :: (Snd ij ~ Fst jk, Fst ij ~ Fst ik, Snd jk ~ Snd ik) => a ij -> Thrist a jk -> Thrist a ik
+  (:-) :: (Fst ij ~ i, Snd ij ~ j, Fst ik ~ i, Snd ik ~ k) => a ij -> Thrist a '(j,k) -> Thrist a ik
 
 instance IFunctor Thrist where
   imap _ Nil = Nil
   imap f (r :- rs) = f r :- imap f rs
 
+(+++) :: Thrist a '(i,j) -> Thrist a '(j,k) -> Thrist a '(i,k)
+Nil       +++ bs = bs
+(a :- as) +++ bs = a :- (as +++ bs)
+
 -- instance IApplicative Thrist
 
 instance IMonad Thrist where
   ireturn a = a :- Nil -- we can't determine the correct kind without kind constraints
-  -- ibind _ Nil = Nil
-  -- ibind f (a :- as) = f a >< ibind f as
+  ibind _ Nil = Nil
+  ibind f (a :- as) = herp (derp (f a) +++ derp (ibind f as))
 
 type Unitary k a = k (At a '( '(), '())) '( '(), '())
 
 type List a = Unitary Thrist a
 
 instance IMonoid (Thrist a) where
-  -- imempty = Nil
-  -- Nil      >< bs = bs
-  -- (a :- as) >< bs = a :- (as >< bs)
+  imempty = Nil
+  as >< bs = herp (derp as +++ derp bs)
 
 instance IFoldable Thrist where
-  -- ifoldMap _ Nil = imempty
-  -- ifoldMap f (a :- as) = f a >< ifoldMap f as
+  ifoldMap _ Nil = imempty
+  ifoldMap f (a :- as) = herp (derp (f a) >< derp (ifoldMap f as))
 
